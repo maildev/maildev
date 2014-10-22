a4462225ef01e94dc2a1ec35fd71279d3f743416
Roy Paterson <roy@roypaterson.com>
2014-10-22 12:47:05 -0700

    Update UI at most 5 times per second on new emails

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,9 +20,20 @@ app.controller('MainCtrl', [
       loadData();
     });
 
+    var refreshTimeout = null;
     $rootScope.$on('newMail', function(e, newEmail) {
+      
+      //update model
       $scope.items.push(newEmail);
-      $scope.$apply();
+
+      //update DOM at most 5 times per second
+      if (!refreshTimeout) {
+        refreshTimeout = setTimeout(function() {
+          refreshTimeout = null;
+          $scope.$apply();
+        }, 200);
+      }
+
     });
 
     // Click event handlers

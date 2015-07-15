36d3594b849a825caa32c599efbdd9016a336a0e
Xavier Priour <xavier.priour@bubblyware.com>
2015-07-16 00:49:08 +0200

    Add option to specify web address separately from smtp address

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ module.exports = function(config) {
       .version(version)
       .option('-s, --smtp <port>', 'SMTP port to catch emails [1025]', '1025')
       .option('-w, --web <port>', 'Port to run the Web GUI [1080]', '1080')
-      .option('--ip <ip address>', 'IP Address to bind services to', '0.0.0.0')
+      .option('--ip <ip address>', 'IP Address to bind SMTP service to', '0.0.0.0')
       .option('--outgoing-host <host>', 'SMTP host for outgoing emails')
       .option('--outgoing-port <port>', 'SMTP port for outgoing emails')
       .option('--outgoing-user <user>', 'SMTP user for outgoing emails')
@@ -31,6 +31,7 @@ module.exports = function(config) {
       .option('--outgoing-secure', 'Use SMTP SSL for outgoing emails')
       .option('--incoming-user <user>', 'SMTP user for incoming emails')
       .option('--incoming-pass <pass>', 'SMTP password for incoming emails')
+      .option('--web-ip <ip address>', 'IP Address to bind HTTP service to, defaults to --ip')
       .option('--web-user <user>', 'HTTP user for GUI')
       .option('--web-pass <password>', 'HTTP password for GUI')
       .option('-o, --open', 'Open the Web GUI after startup')
@@ -60,7 +61,7 @@ module.exports = function(config) {
     );
   }
 
-  web.start(config.web, config.ip, mailserver, config.webUser, config.webPass);
+  web.start(config.web, config.webIp ? config.webIp : config.ip, mailserver, config.webUser, config.webPass);
 
   if (config.open){
     var open = require('open');

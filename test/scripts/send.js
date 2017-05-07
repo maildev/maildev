5bacc498b9adb412bfb4030df4c67a73ebabc962
Dan Farrelly <daniel.j.farrelly@gmail.com>
2017-05-07 14:10:25 -0400

    Fix test send script

--- test/scripts/send.js
+++ test/scripts/send.js
@@ -5,14 +5,14 @@
  * Run this to send emails to port 1025 for testing MailDev during development
  *   node test/scripts/send.js
  */
-
-var nodemailer = require('nodemailer');
+const path = require('path')
+const nodemailer = require('nodemailer')
 
 // Create a transport with MailDev's default receiving port
 var transporter = nodemailer.createTransport({
   port: 1025,
   ignoreTLS: true
-});
+})
 
 // Messages list
 var messages = [
@@ -48,12 +48,12 @@ var messages = [
     to: 'Johnny Utah <johnny.utah@fbi.gov>',
     subject: 'The ultimate price',
     text: 'If you want the ultimate, you\'ve got to be willing to pay the ultimate price. \nIt\'s not tragic to die doing what you love.',
-    html: '<!DOCTYPE html><html><head></head><body style="background:#eee;font-family:sans-serif;padding:2em 2em;">' +
+    html: '<!DOCTYPE html><html><head></head><body style="background:#eeefont-family:sans-serifpadding:2em 2em">' +
           '<h1>Point Break</h1>' +
           '<img src="http://farm8.staticflickr.com/7337/11784709785_bbed9bae7d_m.jpg">' +
           '<p>If you want the ultimate, you\'ve got to be willing to pay the ultimate price. <br>It\'s not tragic to die doing what you love.</p>' +
           '<p><strong>- Bodhi</strong></p>' +
-          '</body></html>',
+          '</body></html>'
   },
 
   // Email w/ embedded image
@@ -65,27 +65,22 @@ var messages = [
     attachments: [
       {
         filename: 'tyler.jpg',
-        path: __dirname + '/tyler.jpg',
+        path: path.join(__dirname, '/tyler.jpg'),
         cid: '12345'
       }
     ]
   }
 
-];
+]
 
-
-function sendEmails(logErrors) {
-  messages.forEach(function(message){
-    transporter.sendMail(message, function(err, info) {
-      if (logErrors && err)
-        return console.log('Test email error: ', err);
-      console.log('Test email sent: ' + info.response);
-    });
-  });
+function sendEmails (logErrors) {
+  messages.forEach(function (message) {
+    transporter.sendMail(message, function (err, info) {
+      if (logErrors && err) { return console.log('Test email error: ', err) }
+      console.log('Test email sent: ' + info.response)
+    })
+  })
 }
 
 // Run once if called directly, otherwise export
-if (require.main === module) 
-  sendEmails(true);
-else
-  module.exports = sendEmails;
+if (require.main === module) { sendEmails(true) } else { module.exports = sendEmails }

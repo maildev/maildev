a1da1a783e29b08427f155c7deaf5e709021de66
sebastien <sebastien.bramille@gmail.com>
2019-03-21 16:42:09 +0000

    Add tests

--- test/outgoing.test.js
+++ test/outgoing.test.js
@@ -17,13 +17,69 @@ describe('outgoing', () => {
         expect(err).toNotExist()
         outgoing.setup()
         expect(outgoing.isEnabled()).toBe(true)
+        outgoing.getClient().on('end', () => {
+          smtpserver.close(done)
+        })
         outgoing.close()
-        smtpserver.close(done)
       })
     })
   })
 
   describe('relayMail', () => {
+    it('should set auto relay mode without an initialised client', () => {
+      let spy = expect.createSpy()
+      spy = expect.spyOn(require('../lib/logger'), 'info')
+      // Close the SMTP server before doing anything, an investigation is needed to find where the SMTP connection is not closed
+      outgoing.getClient().on('end', () => {
+        outgoing.setAutoRelayMode()
+
+        expect(outgoing.getConfig().autoRelay).toBe(false)
+        expect(spy).toHaveBeenCalledWith('Outgoing mail not configured - Auto relay mode ignored')
+        spy.restore()
+      })
+    })
+
+    it('should set auto relay mode with a wrong rules', (done) => {
+      const rules = 'testrule'
+      let spy = expect.createSpy()
+      spy = expect.spyOn(require('../lib/logger'), 'error')
+      outgoing.setup()
+
+      // TODO: Use the expect toThrow helper, I will need to update the version of the expect library before being able to do it
+      try {
+        outgoing.setAutoRelayMode(true, rules)
+      } catch (e) {
+        expect(e.message).toBe("ENOENT: no such file or directory, open 'testrule'")
+        expect(spy).toHaveBeenCalledWith(`Error reading config file at ${rules}`)
+        spy.restore()
+
+        done()
+      }
+    })
+
+    it('should set an auto relay email address', (done) => {
+      const rules = ['test']
+      const emailAddress = 'test@test.com'
+
+      let spy = expect.createSpy()
+      spy = expect.spyOn(require('../lib/logger'), 'info')
+
+      outgoing.setup()
+      outgoing.setAutoRelayMode(true, rules, emailAddress)
+      const config = outgoing.getConfig()
+      expect(config.autoRelay).toBe(true)
+      expect(config.autoRelayRules).toBe(rules)
+      expect(config.autoRelayAddress).toBe(emailAddress)
+
+      expect(spy).toHaveBeenCalledWith([
+        'Auto-Relay mode on',
+        `Relaying all emails to ${emailAddress}`,
+        `Relay rules: ${JSON.stringify(rules)}`
+      ].join((', ')))
+
+      done()
+    })
+
     it('should send outgoing email', (done) => {
       const port = getPort()
       const email = {

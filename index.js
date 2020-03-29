7ef2e071a24dd89c6edc2746cc7726b9ed522734
Dan Farrelly <daniel.j.farrelly@gmail.com>
2020-03-29 17:33:53 -0400

    Extract options parsing as library

--- index.js
+++ index.js
@@ -12,41 +12,14 @@ const pkg = require('./package.json')
 const web = require('./lib/web')
 const mailserver = require('./lib/mailserver')
 const logger = require('./lib/logger')
+const { options, appendOptions } = require('./lib/options')
 
 module.exports = function (config) {
   const version = pkg.version
 
   if (!config) {
     // CLI
-    config = program
-      .version(version)
-      .option('-s, --smtp <port>', 'SMTP port to catch emails [1025]', process.env.MAILDEV_SMTP_PORT || '1025')
-      .option('-w, --web <port>', 'Port to run the Web GUI [1080]', process.env.MAILDEV_WEB_PORT || '1080')
-      .option('--https', 'Switch from http to https protocol', !!process.env.MAILDEV_HTTPS)
-      .option('--https-key <file>', 'The file path to the ssl private key', process.env.MAILDEV_HTTPS_KEY)
-      .option('--https-cert <file>', 'The file path to the ssl cert file', process.env.MAILDEV_HTTPS_CERT)
-      .option('--ip <ip address>', 'IP Address to bind SMTP service to', process.env.MAILDEV_IP || '0.0.0.0')
-      .option('--outgoing-host <host>', 'SMTP host for outgoing emails', process.env.MAILDEV_OUTGOING_HOST)
-      .option('--outgoing-port <port>', 'SMTP port for outgoing emails', process.env.MAILDEV_OUTGOING_PORT)
-      .option('--outgoing-user <user>', 'SMTP user for outgoing emails', process.env.MAILDEV_OUTGOING_USER)
-      .option('--outgoing-pass <password>', 'SMTP password for outgoing emails', process.env.MAILDEV_OUTGOING_PASS)
-      .option('--outgoing-secure', 'Use SMTP SSL for outgoing emails', !!process.env.MAILDEV_OUTGOING_SECURE)
-      .option('--auto-relay [email]', 'Use auto-relay mode. Optional relay email address', process.env.MAILDEV_AUTO_RELAY)
-      .option('--auto-relay-rules <file>', 'Filter rules for auto relay mode', process.env.MAILDEV_AUTO_RELAY_RULES)
-      .option('--incoming-user <user>', 'SMTP user for incoming emails', process.env.MAILDEV_INCOMING_USER)
-      .option('--incoming-pass <pass>', 'SMTP password for incoming emails', process.env.MAILDEV_INCOMING_PASS)
-      .option('--web-ip <ip address>', 'IP Address to bind HTTP service to, defaults to --ip', process.env.MAILDEV_WEB_IP)
-      .option('--web-user <user>', 'HTTP user for GUI', process.env.MAILDEV_WEB_USER)
-      .option('--web-pass <password>', 'HTTP password for GUI', process.env.MAILDEV_WEB_PASS)
-      .option('--base-pathname <path>', 'base path for URLs', process.env.MAILDEV_BASE_PATHNAME)
-      .option('--disable-web', 'Disable the use of the web interface. Useful for unit testing', !!process.env.MAILDEV_DISABLE_WEB)
-      .option('--hide-extensions <extensions>',
-        'Comma separated list of SMTP extensions to NOT advertise (SMTPUTF8, PIPELINING, 8BITMIME)',
-        function (val) { return val.split(',') }
-      )
-      .option('-o, --open', 'Open the Web GUI after startup')
-      .option('-v, --verbose')
-      .option('--silent')
+    config = appendOptions(program.version(version), options)
       .parse(process.argv)
   }
 

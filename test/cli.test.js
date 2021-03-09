8355ab89859ac23db05f6205aa07f9532623d75f
DominikSerafin <DominikSerafin@users.noreply.github.com>
2021-03-09 16:01:23 +0100

    Revert changes to cli tests

--- test/cli.test.js
+++ test/cli.test.js
@@ -8,24 +8,20 @@ const bin = path.join(__dirname, '../bin/maildev')
 
 describe('cli', () => {
   it('should shutdown with SIGTERM signal', (done) => {
-    const maildev = spawn(bin, [], { cwd: undefined, env: process.env, shell: true })
+    const maildev = spawn(bin)
     maildev.on('close', (code, signal) => {
       expect(signal).toBe('SIGTERM')
       done()
     })
-    maildev.on('spawn', () => {
-      maildev.kill('SIGTERM')
-    })
-  }).timeout(10000)
+    maildev.kill('SIGTERM')
+  })
 
   it('should shutdown with SIGINT signal', (done) => {
-    const maildev = spawn(bin, [], { cwd: undefined, env: process.env, shell: true })
+    const maildev = spawn(bin)
     maildev.on('close', (code, signal) => {
       expect(signal).toBe('SIGINT')
       done()
     })
-    maildev.on('spawn', () => {
-      maildev.kill('SIGINT')
-    })
-  }).timeout(10000)
+    maildev.kill('SIGINT')
+  })
 })

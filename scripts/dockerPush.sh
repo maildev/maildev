0b4d68e0ebe46e9765b9ef3be52dfe5ae920bf29
James Marino <james@marino.io>
2022-12-27 15:32:50 +1100

    feat: added arm v8 to docker build and push

--- scripts/dockerPush.sh
+++ scripts/dockerPush.sh
@@ -3,7 +3,7 @@
 #   jq (https://stedolan.github.io/jq/)
 
 # Build cross platform by default
-DEFAULT_PLATFORM="linux/amd64,linux/arm64"
+DEFAULT_PLATFORM="linux/amd64,linux/arm64,linux/arm/v8"
 PLATFORM="${1:-$DEFAULT_PLATFORM}"
 
 VERSION=`npm version --json | jq -r .maildev`

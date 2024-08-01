0d51881b5293943b2c6bf087f9520e2a17eb5bc0
Timshel <Timshel@users.noreply.github.com>
2024-08-01 10:49:13 +0200

    Remove vendored smt-connection

--- vendor/smtp-connection/lib/smtp-connection.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// See the following for the actual source code:
-// https://github.com/nodemailer/nodemailer/blob/master/lib/smtp-connection/index.js
-
-const SMTPConnection = require('nodemailer/lib/smtp-connection');
-
-module.exports = SMTPConnection;

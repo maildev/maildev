7c0a16c583507e4c24f8f7ca2dd00291f6fdce5c
Dan Farrelly <djfarrelly@users.noreply.github.com>
2024-12-10 16:58:21 -0500

    Hash filename to prevent file name injections via Content-ID header. (#508)
    * Hash filename to prevent file name injections via Content-ID header.
    
    Fixes CVE-2024-27448. Fixes #487.
    
    * Add test to cover CVE-2024-27448

--- test/email.test.js
+++ test/email.test.js
@@ -8,6 +8,7 @@ const assert = require('assert')
 const fs = require('fs')
 const http = require('http')
 const path = require('path')
+const crypto = require('crypto')
 const nodemailer = require('nodemailer')
 
 const MailDev = require('../index.js')
@@ -150,54 +151,71 @@ describe('email', () => {
   })
 
   it('should handle embedded images with cid', async () => {
+    const prefix = 'test-cid-replacement';
+    const cid = 'image';
     const emailsForTest = [
       {
         from: 'johnny.utah@fbi.gov',
         to: 'bodhi@gmail.com',
-        subject: 'Test cid replacement #1',
-        html: '<img src="cid:image"/>',
+        subject: `${prefix}-1`,
+        html: `<img src="cid:${cid}"/>`,
         attachments: [
           {
             filename: 'tyler.jpg',
             path: path.join(__dirname, 'tyler.jpg'),
-            cid: 'image'
+            cid,
           }
         ]
       },
       {
         from: 'johnny.utah@fbi.gov',
         to: 'bodhi@gmail.com',
-        subject: 'Test cid replacement #2',
-        html: '<img src="cid:image"/>',
+        subject: `${prefix}-2`,
+        html: `<img src="cid:${cid}"/>`,
         attachments: [
           {
             filename: 'wave.jpg',
             path: path.join(__dirname, 'wave.jpg'),
-            cid: 'image'
+            cid,
           }
         ]
       }
     ]
-
     let receivedEmails = 0;
 
     return new Promise((resolve, reject) => {
       maildev.on('new', (email) => {
+        // Filter out other events for other tests
+        if (!email.subject.startsWith(prefix)) {
+          return ;
+        }
         // Simple replacement to root url
         maildev.getEmailHTML(email.id, (_, html) => {
-          const attachmentFilename = (email.subject.endsWith('#1')) ? 'tyler.jpg' : 'wave.jpg'
+          const attachmentFilename = (email.subject.endsWith('1')) ? 'tyler.jpg' : 'wave.jpg'
+          // The filename is hashed based on content id
+          const hashedFilename = function(fileName){
+            return path.format({
+              name: crypto.createHash('md5').update(cid).digest('hex'),
+              ext: path.extname(fileName) })
+          }(attachmentFilename);
           const contentWithoutNewLine = html.replace(/\n/g, '')
-          assert.strictEqual(contentWithoutNewLine, '<html><head></head><body><img src="/email/' + email.id + '/attachment/' + attachmentFilename + '"></body></html>')
-          const host = `${defaultMailDevOpts.ip}:${web}`
-          const attachmentLink = `${host}/email/${email.id}/attachment/${attachmentFilename}`
+          const httpFilePathName = `/email/${email.id}/attachment/${hashedFilename}`
+          assert.strictEqual(contentWithoutNewLine, '<html><head></head><body><img src="'+ httpFilePathName + '"></body></html>')
 
-          // Pass baseUrl
+          // Test host file prefixing:
+          const host = `${defaultMailDevOpts.ip}:${web}`
+          const attachmentUrl = `${host}${httpFilePathName}`
+
+          // Pass baseUrl (host)
           maildev.getEmailHTML(email.id, host, (_, html) => {
             const contentWithoutNewLine = html.replace(/\n/g, '')
-            assert.strictEqual(contentWithoutNewLine, `<html><head></head><body><img src="//${attachmentLink}"></body></html>`)
+            try {
+            assert.strictEqual(contentWithoutNewLine, `<html><head></head><body><img src="//${attachmentUrl}"></body></html>`,
+              'attachment url should be prefixed with host ip and port and protocol-agnostic //'
+            )} catch(err) { reject(err)}
 
             // Check contents of attached/embedded files
-            http.get(`http://${attachmentLink}`, (res) => {
+            http.get(`http://${attachmentUrl}`, (res) => {
               if (res.statusCode !== 200) {
                 return reject(new Error('Failed to get attachment: ' + res.statusCode))
               }
@@ -223,6 +241,58 @@ describe('email', () => {
       emailsForTest.forEach(async (email) => {
         await transporter.sendMail(email)
       })
+    })
+  })
+
+  it('should not allow attachment cid with malicious path (CVE-2024-27448)', async () => {
+    const cid = '../vuln'
+    const filename = 'tyler.jpg'
+    const subject = 'test-CVE-2024-27448'
+    const testEmail = {
+      from: 'johnny.utah@fbi.gov',
+      to: 'bodhi@gmail.com',
+      subject,
+      html: `<img src="cid:${cid}"/>`,
+      attachments: [
+        {
+          filename,
+          path: path.join(__dirname, filename),
+          cid,
+        }
+      ]
+    }
+
+    const hashedFilename = path.format({
+      name: crypto.createHash('md5').update(cid).digest('hex'),
+      ext: path.extname(filename)
+    })
+
+    return new Promise((resolve, reject) => {
+      maildev.on('new', (email) => {
+        // Filter out other events for other tests
+        if (email.subject !== subject) {
+          return;
+        }
+        // Simple replacement to root url
+        maildev.getEmailHTML(email.id, (_, html) => {
+          const contentWithoutNewLine = html.replace(/\n/g, '')
+          const httpFilePathName = `/email/${email.id}/attachment/${hashedFilename}`
+          try {
+            assert.strictEqual(contentWithoutNewLine, '<html><head></head><body><img src="'+ httpFilePathName + '"></body></html>',
+              'it should contain the properly hashed cid filename'
+            )
+            // Also tests getAttachmentFilePath in this integration test
+            const filePath = path.join(maildev.mailDir, email.id, hashedFilename)
+            const isFileAtCorrectPath = fs.existsSync(filePath)
+            assert.ok(isFileAtCorrectPath, 'attachment file was created at the correct location within the temp directory')
+            resolve();
+          } catch (err) {
+            return reject(err);
+          }
+        })
+      })
+
+      transporter.sendMail(testEmail)
     })
   })
 })
